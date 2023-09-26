87015a0ca760cc14067ff96a7b70eb4dac78de5f
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-26 16:15:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const uuid = require("uuid");
+
+
+const livros = [];
+
+
+const livrosRouter = express.Router();
+
+livrosRouter.get("/", (req, res) => {
+  res.json(livros);
+});
+
+
+livrosRouter.post("/", (req, res) => {
+  const livro = {
+    id: uuid.v4(),
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    autor: req.body.autor,
+  };
+
+  livros.push(livro);
+
+  res.status(201).json(livro);
+});
+
+
+const app = express();
+app.use(express.json())
+app.use("/livros", livrosRouter);
+app.listen(3000);
