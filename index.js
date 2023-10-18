1a3a2e8d5172ee79e6bea825eef0bbd7cae759bb
ToScano23 <ftmessias23@gmail.com>
2023-10-17 23:56:42 -0300

    PUT function now working

--- index.js
+++ index.js
@@ -28,16 +28,22 @@ livrosRouter.post("/", (req, res) => {
 
 //PUT
 livrosRouter.put("/", (req, res) => {
-  const livro = {
-    id: req.body.id,
-    titulo: req.body.titulo,
-    edicao: req.body.edicao,
-    autor: req.body.autor,
-  };
+  //Id do livro no corpo da requisição
+  const livroId = req.body.id;
+  //Procura o livro
+  const livro = livros.find((livro)=> livro.id === livroId);
+  
+  //Livro não encontrado
+  if (!livro) {
+    return res.status(404).json({ error: "Livro não encontrado! Verifique o id" });
+  }
+  
+  //Atualizar as propriedades do livro, conforme id e requisição
+  livro.titulo = req.body.titulo;
+  livro.edicao = req.body.edicao;
+  livro.autor = req.body.autor;
 
-  livros.put(livro);
-
-  res.status(201).json(livro);
+  res.status(200).json(livro);
 });
 
 //DELETE
